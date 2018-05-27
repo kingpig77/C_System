0d1f170a25d268833a71b3aa7c75c688eea1b399
Reim <33375067+kingpig77@users.noreply.github.com>
2018-05-27 20:01:24 +0900

    First

---
+++ MBCS_WBCS1.cpp
@@ -0,0 +1,14 @@
+#define UNICODE
+#define _UNICODE
+
+#include<stdio.h>
+#include<tchar.h>
+#include<windows.h>
+
+int wmain(void)
+{
+	TCHAR str[] = _T("1234567");
+	int size = sizeof(str);
+	printf("string length : %d \n", size);
+	return 0;
+}
---
+++ WBCS2.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<locale.h>
+
+int main(void)
+{
+	_wsetlocale(LC_ALL, L"korean");	
+	wchar_t str[] = L"ABC";
+	int size = sizeof(str);
+	int len = wcslen(str);
+	
+	wprintf(L"Array Size : %d \n", size);
+	wprintf(L"문자열 Length : %d \n", len);
+	
+	system("pause");
+}
